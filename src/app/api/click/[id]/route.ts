d1383eb898280b560d238c8b02670b80db2045bd
Santino <donnolasantino@gmail.com>
2026-04-17 01:05:22 -0300

    Force dynamic APIs

--- src/app/api/click/[id]/route.ts
+++ src/app/api/click/[id]/route.ts
@@ -2,6 +2,8 @@ import { prisma } from "@/lib/prisma";
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(
     request: NextRequest,
     context: { params: Promise<{ id: string }> } // Type adjusted for Next.js 15+ 
